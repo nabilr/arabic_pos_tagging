ec6afe3aae8b3c283800b7901e93ca3aaaa91624
nabilr <nabil.rahiman@gmail.com>
2020-11-16 13:46:48 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # arabic_pos_tagging
 Arabic Parts of Speech Taging
+
+## Down load Arabic Corpus from https://catalog.ldc.upenn.edu/LDC2010T13
+Name of the file download atb1_v4_1_LDC2010T13.tgz
+Copy th extracted folder atb1_v4_1 into flder data/
+Follder used to train the model: atb1_v4_1/data/pos/after/
+
+## PATB file format 
+ INPUT STRING: الولايات
+     IS_TRANS: AlwlAyAt
+      COMMENT: []
+        INDEX: P1W4
+      OFFSETS: 12,21
+  UNVOCALIZED: AlwlAyAt
+    VOCALIZED: Al+wilAy+At+u
+          POS: DET+NOUN+NSUFF_FEM_PL+CASE_DEF_NOM
+        GLOSS: the + states/provinces + [fem.pl.] + [def.nom.]
+
+Every word is described in 9 lines.
+
+IS_TRANS:   Arabic pronounciation
+INDEX:      Number after P show, sentance index and number after w indicates word position
+OFFSETS:    It idicates the length and word index position in the document.
+
+
+
+## Stage 1: Preprocess the data:
+Min sentence size: 7 
+Max sentence size: 134 
+Avg sentence size: 4.774033885398819
+
+Total number of sentence to train: 5253
+
+### Construct sentence structure.
+
+Read all the files and construct a sentance structure and associated word information from PATB file
+
+
+### Run
+
+
---
+++ atb_to_sentance_format.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import json
+from camel_tools.morphology.database import MorphologyDB
+from camel_tools.morphology.analyzer import Analyzer
+
+base_dir = 'data/atb1_v4_1/data/pos/after/'
+
+
+def get_tag(tagset):
+    s = ''
+
+    for t in tagset.split('+'):
+        if t == '':
+            continue
+        i = t.index('/') + 1
+        t = t[i:]
+        s += '+' + t
+    
+    return s.strip('+')
+
+
+files = os.listdir(base_dir)
+
+sentence_size = 0
+min_sentence = 1000
+max_sentence = 0
+
+# Construct sentence from the PATB file format
+sentences = []
+for f in files:
+
+    path = base_dir + '/' + f
+    # print(path)
+    fd = open(path)
+
+    lines = fd.readlines()
+    pairs = [[item[0].strip(), item[1].strip()] for item in [t.strip().split(':')  for t in lines if t.strip() != '']]
+    
+    sentence = []
+    for i in range(0, len(pairs), 9):
+        k_v = {item[0]:item[1] for item in   pairs[i: i + 9]}
+        # print(k_v)
+        
+        if int(k_v['INDEX'].split('W')[1]) == 1:
+            # print(k_v['INDEX'], k_v['INDEX'].split('W')[1])
+            sentence = []
+            sentences.append(sentence)
+             
+        sentence.append(k_v)
+
+    if len(sentence) != 0:
+        sentences.append(sentence)
+
+    s_size = len(sentences[-1])
+    if s_size < min_sentence:
+        min_sentence = s_size
+
+    if s_size > max_sentence:
+        max_sentence = s_size
+
+    sentence_size += s_size
+    
+
+    fd.close()
+    
+
+
+print(min_sentence, max_sentence, sentence_size/len(sentences))
+
+# Extract Morphological properties of every word from corpus
+
+db = MorphologyDB.builtin_db()
+analyzer = Analyzer(db)
+
+# # Create analyzer with NOAN_PROP backoff
+# analyzer = Analyzer(db, 'NOAN_PROP')
+
+training_set = []
+
+for sentence in sentences[0:10]:
+    s = []
+    for word in sentence:
+        
+        analyses = analyzer.analyze(word['INPUT STRING'])
+        # print(word, analyses)
+        for d in analyses:
+            # print(get_tag(d['bw']) == sentences[0][0]['POS'])
+            tag = get_tag(d['bw'])
+            if  tag == word['POS']:
+
+                tag_set =[[k,d[k]] for  k in d]
+                s.append([word['IS_TRANS'], d])
+                break
+                # print(d)
+
+
+        i += 1    
+
+    training_set.append(s)  
+
+
+
+# print(training_set[0])
+
+print(len(sentences), len(training_set))
+
+
+with open('data/preprocess.json', 'w') as outfile:
+    json.dump(training_set, outfile)
+
+# for d in analyses:
+#     print(get_tag(d['bw']) == sentences[0][0]['POS'])
+    
+
+
+
+
+
